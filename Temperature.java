f3ddb0470ec4666cd9c4053b74c313b3720af841
watzoo <116692328+watzoo@users.noreply.github.com>
2022-10-25 19:52:49 -0700

    Add files via upload

--- Temperature.java
+++ Temperature.java
@@ -1,11 +1,11 @@
 
 // Author(s): Daniel Lubin 
-// Date of Last Modification: 10/10/2022
+// Date of Last Modification: 10/25/2022
 // Course: CS111B 
 // Instructor: C. Conner 
-// Assignment #4
-// File Name: CS111Boxes.java
-// This program will display 10 boxes alternating white and black with 1 random red box
+// Assignment #5
+// File Name: Temperature.java
+// This program represents temperatures in either farenheight or celcius.
 
 public class Temperature implements TemperatureInterface
 {
@@ -13,39 +13,36 @@ public class Temperature implements TemperatureInterface
     private double degree;
     private char scale;
     
-    //constructors
+    /*Constructor Methods. */
     public Temperature(){
     degree = 0;
     scale = 'C';
     }
-    
     public Temperature(double degree){
     this.degree = degree;
     scale = 'C';
     }
-    
     public Temperature(char scale){
     degree = 0;
     this.scale = scale;
     }
-    
     public Temperature(double degree, char scale){
     this.degree = degree;
     this.scale = scale;
     }
 
+    /*Write outputs */
     public void writeOutput() {
     System.out.println("Temperature: "+degree+" "+scale);
     }
-
     public void writeC(){
     System.out.print("Temperature : "+fahrenheightToCelcius()+ " C");
     }
-
     public void writeF(){
     System.out.print("Temperature : "+celciusToFarenheight()+ " F");
     }
 
+    /*Methods for getting temps */
     public double getC(){
         return fahrenheightToCelcius();
     }
@@ -53,37 +50,34 @@ public class Temperature implements TemperatureInterface
         return celciusToFarenheight();
     }
 
+    /* Calculations for converting between scales */
     public double fahrenheightToCelcius() {
     if(scale == 'C' || scale == 'c')
     return ((double)(Math.round(degree*10)))/10;
     else
     return ((double)Math.round((((degree-32)*5)/9)*10))/10;   
-
     }
-
     public double celciusToFarenheight() {
     if(scale == 'F' || scale == 'f')
     return ((double)Math.round(degree*10))/10;
     else
     return ((double)Math.round((1.8*degree + 32)*10))/10;
-
-
     }
+
+
+     /* Set commands for setting the degree and scale sperately as well as all at once.*/
     public void set(double degree){
     this.degree = degree;
     }
-    
     public void set(char scale){
     this.scale = scale;
     }
-    
     public void set(double degree, char scale){
     this.degree = degree;
     this.scale = scale;
     }
 
-
-
+    /*Equals comparison method */
     public boolean equals(Temperature obj){
     if(fahrenheightToCelcius() == obj.fahrenheightToCelcius())
     return true;
--- TemperatureInterface.java
+++ TemperatureInterface.java
@@ -1,31 +1,24 @@
 
-/*
-*Algorithm
-1. Create box dimensions and starting location
-2. Create a random number 
-3. Dispaly boxes alternating black and white
-4. Display 1 red box in the sequence where the random number is
-*/
+
 // Author(s): Daniel Lubin 
-// Date of Last Modification: 10/10/2022
+// Date of Last Modification: 10/25/2022
 // Course: CS111B 
 // Instructor: C. Conner 
-// Assignment #4
-// File Name: CS111Boxes.java
-// This program will display 10 boxes alternating white and black with 1 random red box
-
+// Assignment #5
+// File Name: TemperatureInterface.java
+// This program is an interface for Temperature.
 
 
 
 public interface TemperatureInterface
 {
-    public void writeC();
+    public abstract void writeC();
 
-    public void  writeF();
+    public abstract void  writeF();
 
-    public double getC();
+    public abstract double getC();
 
-    public double getF();
+    public abstract double getF();
 
    
 }
